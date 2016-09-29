454cebeafde973aa9e748b5c3f546dd4098067dd
ice <1033986238@qq.com>
2016-09-29 10:35:23 +0800

    项目工程-02 增加的音效模块

---
+++ Project/Assets/DeadLifeMain/Scripts/AudioModule/AudioController.cs
@@ -0,0 +1,57 @@
+﻿//********************************************************
+//
+// 斌哥的代码
+//
+// CreateTime ：#CreateTime#
+//********************************************************
+
+
+using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour
+{
+    private PlayerAudioAbstract m_playerAudioScript;
+
+    private SceneAudioAbstract m_sceneAudioScript;
+
+    void Start()
+    {
+        m_playerAudioScript = GameObject.FindWithTag(Tags.m_playerAudio).GetComponent<PlayerAudioAbstract>(); ;
+        m_sceneAudioScript = GameObject.FindWithTag(Tags.m_sceneAudio).GetComponent<SceneAudioAbstract>();
+    }	
+
+    /// <summary>
+    /// 返回角色音效播放器
+    /// </summary>
+    /// <returns></returns>
+    public PlayerAudioAbstract GetPlayerAudioScript()
+    {
+        if(m_playerAudioScript!=null)
+        {
+            return m_playerAudioScript;
+        }
+        else
+        {
+            print("PlayerAudioAbstract方法中返回空的m_playerAudioScript");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// 返回场景音效播放器
+    /// </summary>
+    /// <returns></returns>
+    public SceneAudioAbstract GetSceneAudioAbstract()
+    {
+        if (m_sceneAudioScript != null)
+        {
+            return m_sceneAudioScript;
+        }
+        else
+        {
+            print("SceneAudioAbstract方法中返回空的m_sceneAudioScript");
+            return null;
+        }
+    }
+}
---
+++ Project/Assets/DeadLifeMain/Scripts/Tools/Tags.cs
@@ -0,0 +1,23 @@
+﻿//********************************************************
+//
+// 斌哥的代码
+//
+// CreateTime ：#CreateTime#
+//********************************************************
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Tags
+{
+    /// <summary>
+    /// Player音效播放器
+    /// </summary>
+    public const string m_playerAudio = "PlayerAudio";
+
+    /// <summary>
+    /// Scene音效播放器
+    /// </summary>
+    public const string m_sceneAudio = "SceneAudio";
+}
