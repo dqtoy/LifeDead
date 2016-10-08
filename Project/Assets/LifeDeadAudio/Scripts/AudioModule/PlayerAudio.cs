7fc01c4a2235a35619166c263e0c60704847b95e
maruliang <627071866@qq.com>
2016-10-08 20:45:34 +0800

    音效

---
+++ Project/Assets/LifeDeadAudio/Scripts/AudioModule/PlayerAudio.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAudio : PlayerAudioAbstract 
+{
+
+	private AudioClip Girl_Death;
+	private AudioClip Man_Death;
+	private AudioClip Walk;
+	private AudioClip Knife;
+	private AudioClip Jump;
+	private AudioClip Laser;
+	private AudioSource PlayerAudios;
+	public void Start(){
+		PlayerAudios =GameObject.Find("PlayerAudio").GetComponent<AudioSource> ();
+	}
+
+	public override void PlayJumpAudio(){
+		PlayerAudios.clip = Resources.Load ("jump")as AudioClip;
+		PlayerAudios.Play ();
+	}
+	public override void PlayGirlDeadAudio(){
+		PlayerAudios.clip = Resources.Load ("Girl_Death")as AudioClip;
+		PlayerAudios.Play ();
+	}
+	public override void PlayManDeadAudio (){
+		PlayerAudios.clip =Resources.Load ("Man_Death")as AudioClip;
+		PlayerAudios.Play ();
+	}
+	public override void PlayWalkAudio(){
+		PlayerAudios.clip = Resources.Load ("walk")as AudioClip;
+		PlayerAudios.Play ();
+	}
+	public override void PlayLaserAudio(){
+		PlayerAudios.clip = Resources.Load ("laser")as AudioClip;
+		PlayerAudios.Play ();
+	}
+	public override void PlayKnifeAudio(){
+		PlayerAudios.clip = Resources.Load ("knife")as AudioClip;
+		PlayerAudios.Play ();
+	}
+}
---
+++ Project/Assets/LifeDeadAudio/Scripts/AudioModule/SceneAudio.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneAudio : SceneAudioAbstract {
+	private AudioClip ForestBGM;
+	private AudioClip DBGM;
+	private AudioClip Login;
+	private AudioClip SummerBGM;
+	private AudioClip SwampBGM;
+	private AudioSource SceneAudios;
+	void Start(){
+		SceneAudios = GameObject.Find ("SceneAudio").GetComponent<AudioSource> ();
+	}
+	public override void PlayLoginBGAudio(){
+		SceneAudios.clip = Resources.Load ("Login")as AudioClip;
+		SceneAudios.Play ();
+
+	}
+	public override void PlayBGAudio(int level){
+		switch (level) {
+		case 1:
+			SceneAudios.clip = Resources.Load ("SwampBGM")as AudioClip;
+			SceneAudios.Play ();
+			SceneAudios.loop = true;
+			break;
+		case 2:
+			SceneAudios.clip= Resources.Load ("SummerBGM")as AudioClip;
+			SceneAudios.Play ();
+			SceneAudios.loop = true;
+			break;
+		case 3:
+			SceneAudios.clip = Resources.Load ("ForestBGM")as AudioClip;
+			SceneAudios.Play ();
+			SceneAudios.loop = true;
+			break;
+		}
+	}
+	public override void PlayDeadBGAudio(){
+		SceneAudios.clip=Resources.Load ("DBGM")as AudioClip;
+		SceneAudios.Play ();
+		SceneAudios.loop = true;
+	}
+	public void Update(){
+		if (Input.GetMouseButtonDown(0)) {
+			PlayDeadBGAudio();
+		}
+	}
+}
