b6d08364bcaf0cf246c3b344b74c748883d80a11
仝昕 <2422545143@qq.com>
2016-10-09 15:16:39 +0800

    no message

---
+++ Project/Assets/LifeDeadUI/Scripts/LoadHead.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadHead : MonoBehaviour {
+
+    SelectPlayer selectPlayer;
+    private Image m_headImage;
+    
+	void Awake () {
+        m_headImage = this.GetComponent<Image>();
+
+    }
+	
+	
+	void Update () {
+	
+	}
+}
